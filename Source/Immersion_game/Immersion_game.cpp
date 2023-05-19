f2137dcd4518550be7b942bc86972d25d29e9853
Kovalsssky <alkofrex@gmail.com>
2023-05-19 23:20:39 +0300

    start

---
+++ Source/Immersion_game/Immersion_game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Immersion_game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Immersion_game, "Immersion_game" );
---
+++ Source/Immersion_game/Immersion_gameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Immersion_gameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IMMERSION_GAME_API AImmersion_gameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
